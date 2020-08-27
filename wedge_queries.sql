7bc54f26ebddfb0966c4f8b2a52156ea7f4a2cbb
John Chandler <john.chandler@gmail.com>
2020-08-27 17:15:37 -0600

    Create wedge_queries.sql

---
+++ wedge_queries.sql
@@ -0,0 +1,24 @@
+-- Here's a place you to build a table of Sales by Hour
+-- Just a little stub for you to start with
+SELECT * 
+FROM HourSales 
+LIMIT 10 
+
+-- Build a table of Sales by Hour by Year
+
+
+-- Here's a place you to build a table of Sales by Hour for 2015
+
+
+-- Here's a place you to build a table of owner sales by month
+
+-- Build a table of owner sales by month for owners who spent between $5K and $10K
+-- This may require a temporary table. 
+
+
+-- Here's a place you to build a table of the top 10 selling products at the Wedge
+
+
+-- Here's a place you to build a table of the top 10 
+-- selling products at the Wedge by Year 2015 and 2016
+
